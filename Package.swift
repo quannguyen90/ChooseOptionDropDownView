6d04e0be1a9133ab2bdfb109c662b4ecd3d6bf6b
quannv <quannv.tm@gmail.com>
2023-02-06 17:16:27 +0700

    update dependencies SwiftMessage

--- Package.swift
+++ Package.swift
@@ -3,17 +3,36 @@
 
 import PackageDescription
 
+//let package = Package(
+//    name: "ChooseOptionDropDownView",
+//    products: [
+//       .library(name: "ChooseOptionDropDownView", targets: ["ChooseOptionDropDownView"])
+//   ],
+//   targets: [
+//       .target(
+//           name: "ChooseOptionDropDownView",
+//           path: "ChooseOptionDropDownView"
+////           resources: [.process("/Assets/Media.xcassets")]
+////           resources: [.process("/Assets/Media.xcassets")]
+//       )
+//   ]
+//)
 let package = Package(
     name: "ChooseOptionDropDownView",
     products: [
-       .library(name: "ChooseOptionDropDownView", targets: ["ChooseOptionDropDownView"])
-   ],
-   targets: [
-       .target(
-           name: "ChooseOptionDropDownView",
-           path: "ChooseOptionDropDownView"
-//           resources: [.process("/Assets/Media.xcassets")]
-//           resources: [.process("/Assets/Media.xcassets")]
-       )
-   ]
+        .library(name: "ChooseOptionDropDownView", targets: ["ChooseOptionDropDownView"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SwiftKickMobile/SwiftMessages.git", from: "9.0.6")
+    ],
+    targets: [
+        .target(
+            name: "ChooseOptionDropDownView",
+            dependencies: [
+                .product(name: "SwiftMessages", package: "SwiftMessages")
+            ], path: "ChooseOptionDropDownView"
+        )
+    ],
+    swiftLanguageVersions: [.v4, .v5]
 )
+
